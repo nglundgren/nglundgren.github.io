2281f29c4544c2e3538c5a6176491ac48e8e5d94
nglund <ngustavlundgren@gmail.com>
2021-05-04 19:22:02 +0200

    experiments with react

--- react/spa/src/index.js
+++ react/spa/src/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const headerCode = (
+<nav>
+  <a href=".">Link 1</a>
+  <a href=".">Link 2</a>
+  <a href=".">Link 3</a>
+</nav>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const mainCode = (
+  <div>
+    <p>This is where the content goes!</p>
+  </div>
+)
+
+const footerCode = (
+<div>
+  <a href=".">Link 1</a>
+  <a href=".">Link 2</a>
+  <a href=".">Link 3</a>
+</div>
+);
+
+ReactDOM.render(headerCode, document.getElementById('headerEl'));
+ReactDOM.render(mainCode, document.getElementById('mainEl'));
+ReactDOM.render(footerCode, document.getElementById('footerEl'));
